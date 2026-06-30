51072de7cbd9c485963b9b0911cd1c3bbb49a7b9
Guilherme <guilherme.alex.oficial@gmail.com>
2026-06-30 12:34:49 -0300

    chore: bump SW cache para v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'controla-ai-v9';
+const CACHE = 'controla-ai-v10';
 const FILES = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
